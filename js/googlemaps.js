2675e9b9287c2a24576f1f1e97dcd905a1bdcb8e
Ayodeji Adebayo <aadebay2@users.noreply.github.com>
2017-12-26 14:34:37 -0600

    contact page
    changes made to the contact page

--- js/googlemaps.js
+++ js/googlemaps.js
@@ -11,7 +11,8 @@
 
 						// The latitude and longitude to center the map (always required)
 						center: new google.maps.LatLng(35.611011, -97.517921), // BIS
-
+						//load map in satellite view
+						mapTypeId: 'satellite',
 						// How you would like to style the map.
 						// This is where you would paste any style found on Snazzy Maps.
 						styles: [{"featureType":"administrative","elementType":"labels.text.fill","stylers":[{"color":"#444444"}]},{"featureType":"landscape","elementType":"all","stylers":[{"color":"#f2f2f2"}]},{"featureType":"poi","elementType":"all","stylers":[{"visibility":"off"}]},{"featureType":"road","elementType":"all","stylers":[{"saturation":-100},{"lightness":45}]},{"featureType":"road.highway","elementType":"all","stylers":[{"visibility":"simplified"}]},{"featureType":"road.arterial","elementType":"labels.icon","stylers":[{"visibility":"off"}]},{"featureType":"transit","elementType":"all","stylers":[{"visibility":"off"}]},{"featureType":"water","elementType":"all","stylers":[{"color":"#46bcec"},{"visibility":"on"}]}]
