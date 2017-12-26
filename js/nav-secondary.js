d9a7cdd138df0ef619cd1e5bef61fce00215d87a
GrooperGuru <GrooperGuru@users.noreply.github.com>
2017-12-26 09:26:50 -0600

    move css and js files

--- js/nav-secondary.js
+++ js/nav-secondary.js
@@ -1,27 +1,3 @@
-function menuTransition(){
-	$("#menu").css("transition", "margin .3s linear");
-}
-function noScroll(){
-	var checkbox_click = document.getElementById("checkbox_click")
-	if (checkbox_click.checked){
-		$("body").addClass("noScroll");
-		menuTransition();
-		$(".grooper_logo").animate({opacity: 0},100);
-		$("#nav-social").css("marginLeft", "0");
-  } else {
-		$("body").removeClass("noScroll");
-		$("#nav-social").css("marginLeft", "100%");
-		$(".grooper_logo").animate({opacity: 1},200);
-	}
-}
-
-window.onresize = transitionOff;
-function transitionOff(){
-	if (screen.width > 600){
-		menuTransition();
-	}
-}
-
 var SETTINGS = {
   navBarTravelling: false,
   navBarTravelDirection: "",
