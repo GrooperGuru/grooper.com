7df9f6adf98a4395ff5331e44cc55f761ac68867
GrooperGuru <GrooperGuru@users.noreply.github.com>
2018-01-02 19:01:34 -0600

    Nightly Checkin

--- js/nav-primary.js
+++ js/nav-primary.js
@@ -7,17 +7,23 @@ function noScroll(){
 		$("body").addClass("noScroll");
 		menuTransition();
 		$(".grooper_logo").animate({opacity: 0},100);
-		$("#nav-social").css("marginLeft", "0");
+		$("#nav_social").css("marginLeft", "0");
   } else {
 		$("body").removeClass("noScroll");
-		$("#nav-social").css("marginLeft", "100%");
-		$(".grooper_logo").animate({opacity: 1},200);
+		$("#nav_social").css("marginLeft", "100%");
+		$("nav > a").animate({opacity: 1},200); /*grooper logo in priomary nav bar */
 	}
 }
+var resizeTimer;
 
 window.onresize = transitionOff;
 function transitionOff(){
-	if (screen.width > 600){
-		menuTransition();
-	}
+	if ((screen.width > 600) && (document.getElementById("checkbox_click").checked = true)){
+    clearTimeout(resizeTimer);
+    resizeTimer = setTimeout(function(e) {
+      menuTransition();
+      document.getElementById("checkbox_click").click()
+      $("body").removeClass("noScroll");
+    }, 350);
+  }
 }
